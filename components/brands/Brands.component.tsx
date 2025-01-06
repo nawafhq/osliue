9542f6326c95909ef34b2595254563146be8f520
nawafhq <qahtanni.nawaf@yahoo.com>
2025-01-07 02:08:44 +0300

    Change the sliding brands

--- components/brands/Brands.component.tsx
+++ components/brands/Brands.component.tsx
@@ -12,6 +12,16 @@ export default function BrandsComponent() {
         <img src="/brands/microsoft.svg" alt="microsoft" width={2160} height={1080} className="brand-item" />
         <img src="/brands/supabase.svg" alt="supabase" width={2160} height={1080} className="brand-item" />
         <img src="/brands/vercel.svg" alt="vercel" width={2160} height={1080} className="brand-item" />
+
+        {/* Duplicate the images to create a continuous loop */}
+        <img src="/brands/alrajhi.svg" alt="alrajhi" width={2160} height={1080} className="brand-item" />
+        <img src="/brands/auth0.svg" alt="auth0" width={2160} height={1080} className="brand-item" />
+        <img src="/brands/moyasar.svg" alt="moyasar" width={2160} height={1080} className="brand-item" />
+        <img src="/brands/intercom.svg" alt="intercom" width={2160} height={1080} className="brand-item" />
+        <img src="/brands/stc.svg" alt="stc" width={2160} height={1080} className="brand-item" />
+        <img src="/brands/microsoft.svg" alt="microsoft" width={2160} height={1080} className="brand-item" />
+        <img src="/brands/supabase.svg" alt="supabase" width={2160} height={1080} className="brand-item" />
+        <img src="/brands/vercel.svg" alt="vercel" width={2160} height={1080} className="brand-item" />
       </div>
     </div>
   );
